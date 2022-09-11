9e4a3f33a1387e6d19723f65e2c77cdff68383c6
jAcee12 <jaydoncameron80@protonmail.com>
2022-09-11 17:08:43 +1000

    my group menu

--- Assets/Scripts/HandMenuButtonBehaviour.cs
+++ Assets/Scripts/HandMenuButtonBehaviour.cs
@@ -4,24 +4,70 @@ using UnityEngine;
 
 public class HandMenuButtonBehaviour : MonoBehaviour
 {
-    public GameObject menu;
-    private RadialView script;
+    public GameObject navigateMenu;
+    public GameObject myGroupMenu;
+    public GameObject notificationsMenu;
+
+    private GameObject active;
 
     // Start is called before the first frame update
     private void Start()
     {
-        script = menu.GetComponent<RadialView>();
     }
 
     // Update is called once per frame
     private void Update()
     {
     }
-    
-    public void ToggleMenu(bool state)
+
+    private void CloseAll()
+    {
+        if (navigateMenu && navigateMenu.activeSelf)
+        {
+            navigateMenu.SetActive(false);
+            navigateMenu.GetComponent<RadialView>().enabled = false;
+        }
+
+        if (myGroupMenu && myGroupMenu.activeSelf)
+        {
+            myGroupMenu.SetActive(false);
+            myGroupMenu.GetComponent<RadialView>().enabled = false;
+        }
+
+        if (notificationsMenu && notificationsMenu.activeSelf)
+        {
+            notificationsMenu.SetActive(false);
+            notificationsMenu.GetComponent<RadialView>().enabled = false;
+        }
+    }
+
+    public void NavigateMenu(bool state)
+    {
+        if (navigateMenu == null) return;
+        if (state) CloseAll();
+        navigateMenu.SetActive(state);
+        navigateMenu.GetComponent<RadialView>().enabled = state;
+    }
+
+    public void ShowMenu(GameObject menu)
     {
         if (menu == null) return;
-        menu.SetActive(state);
-        menu.GetComponent<RadialView>().enabled = state;
+        if (active != null)
+        {
+            active.SetActive(false);
+            active.GetComponent<RadialView>().enabled = false;
+        }
+
+        active = menu;
+        menu.SetActive(true);
+        menu.GetComponent<RadialView>().enabled = true;
+    }
+
+    public void HideMenu()
+    {
+        if (active == null) return;
+        active.SetActive(false);
+        active.GetComponent<RadialView>().enabled = false;
+        active = null;
     }
 }
