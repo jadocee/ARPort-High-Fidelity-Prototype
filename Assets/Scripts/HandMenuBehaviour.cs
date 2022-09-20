81ce62bdb6fd478ed8dc7b547f7f7e596635ac72
Buddostars <buddo15@outlook.com>
2022-09-20 16:24:39 +1000

    Revert

--- Assets/Scripts/HandMenuBehaviour.cs
+++ Assets/Scripts/HandMenuBehaviour.cs
@@ -1,14 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class HandMenuBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
-    private void Start()
+    void Start()
     {
+        
     }
 
     // Update is called once per frame
-    private void Update()
+    void Update()
     {
+        
     }
 }
