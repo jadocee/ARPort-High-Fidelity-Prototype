5f9a30ac9cb89c4120060f6a2de95f82b7ed02f7
Buddostars <buddo15@outlook.com>
2022-10-03 00:24:52 +1100

    PSA#2
    Description on alert changes

---
+++ Assets/Scripts/DescriptionManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Packages.Rider.Editor.UnitTesting;
+using TMPro;
+using UnityEngine;
+
+
+public class DescriptionManager : MonoBehaviour
+{
+
+
+    public TextMeshPro DescriptionText;
+
+    public string testdescription = "This is a sample Description";
+    // Start is called before the first frame update
+    void Start()
+    {
+        DescriptionText.text = "test: " + testdescription;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
