05e3c99a960e206b71388322f2d88bf417786185
Buddostars <buddo15@outlook.com>
2022-10-09 22:59:35 +1100

    PSA#5
    Worked on making prehabs spawn
    Had huge trouble trying to work out how to actually code it
    It seem like nothing was done but ive been looking up thing to help me make it

--- Assets/Scripts/PSAController.cs
+++ Assets/Scripts/PSAController.cs
@@ -1,17 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using Microsoft.MixedReality.Toolkit.SpatialManipulation;
+using UnityEngine;using UnityEngine.PlayerLoop;
 
 public class PSAController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject psaPrefab;
+    [SerializeField] private GameObject psaContainer;
+
+    public void Awake()
     {
-        
+        if (!psaContainer) 
+        {
+            Debug.Log("PSA Container is missing");
+        }
+
+        if (!psaPrefab)
+        {
+            Debug.Log("PSA Prehab is missing");
+        }
+        psaContainer.SetActive(false);
+        psaContainer.GetComponent<Follow>().enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DisplayPSA(String header, String desc = "")
+    {
+        var psa = Instantiate(psaPrefab, psaContainer.transform.position, psaContainer.transform.rotation);
+        if (psa != null)
+        {
+            var sericeAlert = psa.GetComponent<PSA>();
+            if (sericeAlert != null)
+            {
+                
+            }
+
+            psaContainer.GetComponent<Follow>().enabled = true;
+        }
+    }
+    
+     void Update()
+    {
+        Instantiate(psaPrefab, psaContainer.transform.position, psaContainer.transform.rotation);
+    }
+
+}
+
+
+public class PSA
+{
+    public PSA()
     {
         
     }
