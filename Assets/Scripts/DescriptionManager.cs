c04b235531d7e0a0894632d37d86ab26136739d3
Buddostars <buddo15@outlook.com>
2022-10-12 18:01:51 +1100

    PSA#6
    Clone PSA prehabs spawn when pressing activate psa in hand menu

--- Assets/Scripts/DescriptionManager.cs
+++ Assets/Scripts/DescriptionManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Packages.Rider.Editor.UnitTesting;
@@ -9,32 +10,9 @@ public class DescriptionManager : MonoBehaviour
 {
     private GameObject psaPrefab;
     
-
-    public TextMeshProUGUI DescriptionText1;
-    public TextMeshProUGUI DescriptionText2;
-    public TextMeshProUGUI DescriptionText3;
+    public TextMeshProUGUI DescriptionText;
+    public TextMeshProUGUI Title;
     public TextMeshProUGUI ConfirmedText;
 
-    public string testdescription1 = "This is a sample description";
-    public string testdescription2 = "This is a another alert";
-    public string testdescription3 = "Damn alert 3 is out";
     
-    public string active = "False";
-    // Start is called before the first frame update
-    void Start()
-    {
-        DescriptionText1.text = testdescription1;
-        DescriptionText2.text = testdescription2;
-        DescriptionText3.text = testdescription3;
-        ConfirmedText.text = "The status of the confirmation is " + active;
-
-    }
-    
-
-    // Update is called once per frame
-    void Update()
-    {
-
-
-    }
 }
--- Assets/Scripts/PSAController.cs
+++ Assets/Scripts/PSAController.cs
@@ -1,15 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
 using Microsoft.MixedReality.Toolkit.SpatialManipulation;
-using UnityEngine;using UnityEngine.PlayerLoop;
+using Microsoft.MixedReality.Toolkit.UX;
+using TMPro;
+using UnityEngine;
 
 public class PSAController : MonoBehaviour
 {
     [SerializeField] private GameObject psaPrefab;
     [SerializeField] private GameObject psaContainer;
+    private String header;
+    private String description;
+    
 
-    public void Awake()
+    /*public void Awake()
     {
         if (!psaContainer) 
         {
@@ -22,35 +29,27 @@ public class PSAController : MonoBehaviour
         }
         psaContainer.SetActive(false);
         psaContainer.GetComponent<Follow>().enabled = false;
-    }
+    }*/
+    
 
-    public void DisplayPSA(String header, String desc = "")
+    public void DisplayPsa()
     {
         var psa = Instantiate(psaPrefab, psaContainer.transform.position, psaContainer.transform.rotation);
         if (psa != null)
         {
-            var sericeAlert = psa.GetComponent<PSA>();
-            if (sericeAlert != null)
-            {
-                
-            }
+            psa.SetActive(true);
+            /*psa.GetComponent<DescriptionManager>().Title.text = header;
+            psa.GetComponent<DescriptionManager>().DescriptionText.text = description;
+            psa.transform.SetParent(psaContainer.transform, false);*/
 
-            psaContainer.GetComponent<Follow>().enabled = true;
+            //psaContainer.GetComponent<Follow>().enabled = true;
         }
     }
     
-     void Update()
-    {
-        Instantiate(psaPrefab, psaContainer.transform.position, psaContainer.transform.rotation);
-    }
+    //  void Update()
+    // {
+    //     Instantiate(psaPrefab, psaContainer.transform.position, psaContainer.transform.rotation);
+    // }
 
 }
 
-
-public class PSA
-{
-    public PSA()
-    {
-        
-    }
-}
