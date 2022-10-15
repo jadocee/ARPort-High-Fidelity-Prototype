60ac329ca3a6945b52bec4feb50fa1f7165f86c2
Mi-Genn <mitchellgennette+GHub@gmail.com>
2022-10-15 18:41:59 +1100

    Interaction Tutorial: The "Pinch Me Harder" Update
    -Added the pinch tutorial to finalise the tutorial scene.
    
    -Tried to explain how to perform the pinch gesture from a distance but with terrible wording.
    
    -Added a tutorial endpoint, which can be used to go into the full application. For now, it simply sets the container for the tutorial to an inactive state.

--- Assets/Scripts/TutorialScript.cs
+++ Assets/Scripts/TutorialScript.cs
@@ -12,7 +12,9 @@ public class TutorialScript : MonoBehaviour
     [SerializeField]
     private GameObject HMButton;
     [SerializeField]
-    private GameObject HMB;
+    private GameObject PinchTut;
+    [SerializeField]
+    private GameObject TutContainer;
     private bool HMCancel;
     private Dialog CurrentD;
     private void Awake() //Creates the introduction dialog on awake
@@ -23,7 +25,7 @@ public class TutorialScript : MonoBehaviour
             var toasterScript = toaster.GetComponent<DialogController>();
             if (toasterScript != null)
             {
-                toasterScript.OpenOkayDialog("<size=0.1>Welcome to the ARPort AR Tutorial</size>", "<size=0.06>This tutorial is designed to teach new Augmented Reality users about the types of interactions performed during AR that are utilised in the ARPort application.</size> <size=0.08>To begin, dismiss this pop-up by tapping the button labelled \"OK\" using your hand.</size>", DialogController.DialogSize.Large, callback: (property) =>
+                toasterScript.OpenOkayDialog("<size=0.09>Welcome to the ARPort AR Tutorial</size>", "<size=0.06>This tutorial is designed to teach new Augmented Reality users about the types of interactions performed during AR that are utilised in the ARPort application.</size>\n\n<size=0.06><b>To begin, dismiss this pop-up by tapping the button labelled <color=orange>\"OK\"</color> using your hand.</b></size>", DialogController.DialogSize.Large, callback: (property) =>
                 {
                     if (property.ResultContext.ButtonType.Equals(DialogButtonType.OK))
                     {
@@ -45,7 +47,7 @@ public class TutorialScript : MonoBehaviour
                 var toasterScript = toaster.GetComponent<DialogController>();
                 if (toasterScript != null)
                 {
-                CurrentD = toasterScript.OpenDialog("Using the Hand Menu", "In the ARPort application, objects and menus can appear based on the gesture you're making with your hand. Try positioning your left hand flat with your palm facing towards you. Upon successfully performing the gesture a button labelled \"Complete Task\" will appear. Press this button to proceed with the tutorial.");
+                CurrentD = toasterScript.OpenDialog("<size=0.09>Using the Hand Menu</size>", "<size=0.06>In the ARPort application, objects and menus can appear based on the gesture you're making with your hand.</size>\n\n<size=0.06><b>Try positioning your left hand flat with your palm facing towards you. Upon successfully performing the gesture a button labelled <color=orange>\"Complete Task\"</color> will appear, which you can press to proceed with the tutorial.</b></size>", DialogController.DialogSize.Large);
                 }
             }
     }
@@ -54,6 +56,7 @@ public class TutorialScript : MonoBehaviour
         Debug.Log("Dismissed");
         Destroy(CurrentD.gameObject);
         PinchTutorial();
+        PinchTut.SetActive(true);
         HMButton.SetActive(false);
     }
 
@@ -66,15 +69,14 @@ public class TutorialScript : MonoBehaviour
             var toasterScript = toaster.GetComponent<DialogController>();
             if (toasterScript != null)
             {
-                toasterScript.OpenOkayDialog("Moving Menus with the Pinch Gesture", "", callback: (property) =>
-                {
-                    if (property.ResultContext.ButtonType.Equals(DialogButtonType.OK))
-                    {
-                        Debug.Log("Dismissed");
-                        Destroy(property.TargetDialog.gameObject);
-                    }
-                });
-            }
+            CurrentD = toasterScript.OpenDialog("<size=0.09>Moving Menus with the Pinch Gesture</size>", "<size=0.06>In the ARPort application, you are able to perform a pinch gesture to move menus around the display</size>\n\n<size=0.06><b>Locate the pinch menu and move it closer to your position. To perform this action from a distance, open your palm towards the bar beneath the menu and pinch once the line that originates from your hand ends in a circle, drag it towards you by bringing your hand back. Press the <color=orange>\"X\"</color> button on the Pinch Menu to complete the tutorial", DialogController.DialogSize.Large);
+            } 
         }
+    }
+
+    public void EndTutorial() {
+        Debug.Log("Dismissed");
+        Destroy(CurrentD.gameObject);
+        TutContainer.SetActive(false);
     }
 }
