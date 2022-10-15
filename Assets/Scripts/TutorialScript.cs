345006c8a19ebc5b41adfe6c5593a12b71b8b970
jAcee12 <jaydoncameron80@protonmail.com>
2022-10-15 20:21:57 +1100

    Changed positioning of pinch menu dialog

--- Assets/Scripts/TutorialScript.cs
+++ Assets/Scripts/TutorialScript.cs
@@ -4,6 +4,7 @@ using Microsoft.MixedReality.Toolkit.UX;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Microsoft.MixedReality.Toolkit.SpatialManipulation;
 using ToastNotifications;
 using Unity.VectorGraphics;
 using UnityEngine;
@@ -11,14 +12,14 @@ using UnityEngine.SceneManagement;
 
 public class TutorialScript : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject HMButton;
-    [SerializeField]
-    private GameObject PinchTut;
-    [SerializeField]
-    private GameObject TutContainer;
+    [SerializeField] private GameObject HMButton;
+    [SerializeField] private GameObject PinchTut;
+    [SerializeField] private GameObject TutContainer;
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private GameObject pinchMenu;
     private bool HMCancel;
     private Dialog CurrentD;
+
     private void Awake() //Creates the introduction dialog on awake
     {
         var toaster = GameObject.FindGameObjectWithTag("DialogController");
@@ -27,16 +28,18 @@ public class TutorialScript : MonoBehaviour
             var toasterScript = toaster.GetComponent<DialogController>();
             if (toasterScript != null)
             {
-                toasterScript.OpenOkayDialog("<size=0.09>Welcome to the ARPort AR Tutorial</size>", "<size=0.06>This tutorial is designed to teach new Augmented Reality users about the types of interactions performed during AR that are utilised in the ARPort application.</size>\n\n<size=0.06><b>To begin, dismiss this pop-up by tapping the button labelled <color=orange>\"OK\"</color> using your hand.</b></size>", DialogController.DialogSize.Large, callback: (property) =>
-                {
-                    if (property.ResultContext.ButtonType.Equals(DialogButtonType.OK))
+                toasterScript.OpenOkayDialog("<size=0.09>Welcome to the ARPort AR Tutorial</size>",
+                    "<size=0.06>This tutorial is designed to teach new Augmented Reality users about the types of interactions performed during AR that are utilised in the ARPort application.</size>\n\n<size=0.06><b>To begin, dismiss this pop-up by tapping the button labelled <color=orange>\"OK\"</color> using your hand.</b></size>",
+                    DialogController.DialogSize.Large, callback: (property) =>
                     {
-                        Debug.Log("Dismissed");
-                        Destroy(property.TargetDialog.gameObject);
-                        HMButton.SetActive(true);
-                        HandMenuTutorial();
-                    }
-                });
+                        if (property.ResultContext.ButtonType.Equals(DialogButtonType.OK))
+                        {
+                            Debug.Log("Dismissed");
+                            Destroy(property.TargetDialog.gameObject);
+                            HMButton.SetActive(true);
+                            HandMenuTutorial();
+                        }
+                    });
             }
         }
     }
@@ -44,17 +47,20 @@ public class TutorialScript : MonoBehaviour
     public void HandMenuTutorial()
     {
         var toaster = GameObject.FindGameObjectWithTag("DialogController");
-            if (toaster != null)
+        if (toaster != null)
+        {
+            var toasterScript = toaster.GetComponent<DialogController>();
+            if (toasterScript != null)
             {
-                var toasterScript = toaster.GetComponent<DialogController>();
-                if (toasterScript != null)
-                {
-                CurrentD = toasterScript.OpenDialog("<size=0.09>Using the Hand Menu</size>", "<size=0.06>In the ARPort application, objects and menus can appear based on the gesture you're making with your hand.</size>\n\n<size=0.06><b>Try positioning your left hand flat with your palm facing towards you. Upon successfully performing the gesture a button labelled <color=orange>\"Complete Task\"</color> will appear, which you can press to proceed with the tutorial.</b></size>", DialogController.DialogSize.Large);
-                }
+                CurrentD = toasterScript.OpenDialog("<size=0.09>Using the Hand Menu</size>",
+                    "<size=0.06>In the ARPort application, objects and menus can appear based on the gesture you're making with your hand.</size>\n\n<size=0.06><b>Try positioning your left hand flat with your palm facing towards you. Upon successfully performing the gesture a button labelled <color=orange>\"Complete Task\"</color> will appear, which you can press to proceed with the tutorial.</b></size>",
+                    DialogController.DialogSize.Large);
             }
+        }
     }
 
-    public void dismissHM() {
+    public void dismissHM()
+    {
         Debug.Log("Dismissed");
         Destroy(CurrentD.gameObject);
         PinchTutorial();
@@ -71,12 +77,16 @@ public class TutorialScript : MonoBehaviour
             var toasterScript = toaster.GetComponent<DialogController>();
             if (toasterScript != null)
             {
-            CurrentD = toasterScript.OpenDialog("<size=0.09>Moving Menus with the Pinch Gesture</size>", "<size=0.06>In the ARPort application, you are able to perform a pinch gesture to move menus around the display</size>\n\n<size=0.06><b>Locate the pinch menu and move it closer to your position. To perform this action from a distance, open your palm towards the bar beneath the menu and pinch once the line that originates from your hand ends in a circle, drag it towards you by bringing your hand back. Press the <color=orange>\"X\"</color> button on the Pinch Menu to complete the tutorial", DialogController.DialogSize.Large);
-            } 
+                CurrentD = toasterScript.OpenDialog("<size=0.09>Moving Menus with the Pinch Gesture</size>",
+                    "<size=0.06>In the ARPort application, you are able to perform a pinch gesture to move menus around the display</size>\n\n<size=0.06><b>Locate the pinch menu and move it closer to your position. To perform this action from a distance, open your palm towards the bar beneath the menu and pinch once the line that originates from your hand ends in a circle, drag it towards you by bringing your hand back. Press the <color=orange>\"X\"</color> button on the Pinch Menu to complete the tutorial",
+                    DialogController.DialogSize.Large);
+                CurrentD.transform.position = new Vector3((float) (pinchMenu.transform.position.x - 0.25), 0, 0.6f);
+            }
         }
     }
 
-    public void EndPinch() {
+    public void EndPinch()
+    {
         Debug.Log("Dismissed");
         Destroy(CurrentD.gameObject);
         //TutContainer.SetActive(false);
@@ -92,17 +102,18 @@ public class TutorialScript : MonoBehaviour
             var toasterScript = toaster.GetComponent<DialogController>();
             if (toasterScript != null)
             {
-                toasterScript.OpenOkayDialog("<size=0.09>Tutorial Completed</size>", "<size=0.06>You have completed the ARPort Tutorial, please inform the observers to begin testing.</size>\n\n<size=0.06><b>Once the observers give you the okay, please press the <color=orange>\"OK\"</color> button to begin testing.</b></size>", DialogController.DialogSize.Large, callback: (property) =>
-                {
-                    if (property.ResultContext.ButtonType.Equals(DialogButtonType.OK))
+                toasterScript.OpenOkayDialog("<size=0.09>Tutorial Completed</size>",
+                    "<size=0.06>You have completed the ARPort Tutorial, please inform the observers to begin testing.</size>\n\n<size=0.06><b>Once the observers give you the okay, please press the <color=orange>\"OK\"</color> button to begin testing.</b></size>",
+                    DialogController.DialogSize.Large, callback: (property) =>
                     {
-                        Debug.Log("Dismissed");
-                        Destroy(property.TargetDialog.gameObject);
-                        SceneManager.LoadScene("Scenes/" + "GroupScene031022", LoadSceneMode.Single);
-                    }
-                });
+                        if (property.ResultContext.ButtonType.Equals(DialogButtonType.OK))
+                        {
+                            Debug.Log("Dismissed");
+                            Destroy(property.TargetDialog.gameObject);
+                            SceneManager.LoadScene("Scenes/" + "GroupScene031022", LoadSceneMode.Single);
+                        }
+                    });
             }
         }
     }
-    
 }
