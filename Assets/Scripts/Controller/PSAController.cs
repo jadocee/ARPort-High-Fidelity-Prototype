14c221d60201c39eb4e5c609b8d8719168645f66
Buddostars <buddo15@outlook.com>
2022-10-17 00:24:46 +1100

    PSA#11
    Distance of alerts from camera adjusted

--- Assets/Scripts/Controller/PSAController.cs
+++ Assets/Scripts/Controller/PSAController.cs
@@ -8,6 +8,7 @@ namespace Controller
         [SerializeField] private Dialog psaPrefab;
         [SerializeField] private Dialog confirmationPrefab;
         private int i = 1;
+        private string newMessage;
 
 
         public void Start()
@@ -17,15 +18,37 @@ namespace Controller
 
         public void DisplayPsa()
         {
-            if (i < 100)
+            if (i < 5)
             {
                 Dialog.InstantiateFromPrefab(psaPrefab,
                     new DialogProperty("Alert " + i,
-                        "This is an example of an Alert with a choice message for the user",
+                        NewMessage(),
                         DialogButtonHelpers.OK), true, true);
                 i++;
             }
         }
+
+        private string NewMessage()
+        {
+            if (i == 1)
+            {
+                newMessage = " This is the first alert to tell you about some warning";
+            }
+            if (i == 2)
+            {
+                newMessage = " This is the second alert ";
+            }
+            if (i == 3)
+            {
+                newMessage = " This is the third alert but its fine";
+            }
+            if (i == 4)
+            {
+                newMessage = " This is the fourth alert but Gate C is closed";
+            }
+            return newMessage;
+        }
+
 
         public void DisplayConfimation()
         {
